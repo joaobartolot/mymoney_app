14e1d1e0c5419d199bcfa7d4ff38e53770d097b6
João Victor Bartolot <joao.bartolot@calindra.com.br>
2022-07-30 01:41:01 -0300

    wip: trying to create a model serialization

--- lib/src/model/expense.dart
+++ lib/src/model/expense.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:my_money/src/model/base.dart';
 
-class Expense {
+class Expense implements BaseModel {
   Expense({
     this.uid,
     required this.name,
@@ -8,24 +9,30 @@ class Expense {
     this.createdAt,
   });
 
-  Map<String, dynamic> toMap() {
-    return {
-      'name': name,
-      'price': price,
-      'created_at': createdAt,
-    };
-  }
-
   final String? uid;
   String name;
   double price;
   DateTime? createdAt;
 
-  static Expense fromMap(String? uid, Map<String, dynamic> data) {
-    return Expense(
-        uid: uid ?? '',
-        name: data['name'],
-        price: data['price'],
-        createdAt: (data['created_at'] as Timestamp).toDate());
-  }
+  @override
+  Expense.fromMap(String? uid, Map<String, dynamic> data)
+      : uid = uid ?? '',
+        name = data['name'],
+        price = data['price'],
+        createdAt = (data['created_at'] as Timestamp).toDate();
+
+  @override
+  Map<String, dynamic> toMap() => {
+        'name': name,
+        'price': price,
+        'created_at': createdAt,
+      };
+
+  // static Expense fromMap(String? uid, Map<String, dynamic> data) {
+  //   return Expense(
+  //       uid: uid ?? '',
+  //       name: data['name'],
+  //       price: data['price'],
+  //       createdAt: (data['created_at'] as Timestamp).toDate());
+  // }
 }
---
+++ lib/src/util/firesotre_util.dart
@@ -0,0 +1,11 @@
+import 'package:my_money/src/model/base.dart';
+
+class FirestoreUtil {
+  final String collection;
+
+  FirestoreUtil(this.collection);
+
+  Future<T> getDocument<T extends BaseModel>() {
+    T.
+  }
+}
